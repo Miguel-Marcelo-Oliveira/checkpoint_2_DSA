2a6bfb11c5e3eac1c8d92d578d71b333c1f448a7
ericx33 <ericxy2007@gmail.com>
2026-04-23 09:49:53 -0300

    Corrigindo sintaxe da função

--- src/main.c
+++ src/main.c
@@ -1,43 +1,50 @@
 #include<stdio.h>
 
-int a, x, b, c, f, opcao;
-
-void derivada(int fd) {
+void derivada(int a, int x, int b, int c, int f) {
     
     f = a*(x*x) + b*x + c;
-    fd = a*x + b;
+    int fd = a*x + b;
     
-    return ;
+    return fd;
+}
+
+int main(){
+
+int opcao;
+
+
 
     printf("SISTEMA DE MÉDIA FINAL");
     switch (opcao) {
-    case 1:
-    printf("Insira sua pontuação anual");
-    break; 
+        case 1:
+        printf("Insira sua pontuação anual");
+        break; 
     
-    case 2:
-    printf("Calcule sua média");
-    break;
+        case 2:
+        printf("Calcule sua média");
+        break;
 
-    case 3:
-    printf("Verifique sua situação:");
-    break;
+        case 3:
+        printf("Verifique sua situação:");
+        break;
 
-    case 4:
-    printf("Ver o seu resultado");
-    break;
+        case 4:
+        printf("Ver o seu resultado");
+        break;
 
-    case 5:
-    printf("Calcule sua derivada");
-    break;
+        case 5:
+        printf("Calcule sua derivada");
+        break;
 
-    case 6:
-    printf("Sair");
-    break;
+        case 6:
+        printf("Sair");
+        break;
 
-    default:
-    printf("Digite uma das opções acima!");
+        default:
+        printf("Digite uma das opções acima!");
 
-return 0;
+      
+    }
+    return 0; 
 }
 
