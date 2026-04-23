2b6dbc6100d992cb5e5b175fd10497b1107af73b
ericx33 <ericxy2007@gmail.com>
2026-04-23 09:43:43 -0300

    Adicionar menu do sistema

--- src/main.c
+++ src/main.c
@@ -1,12 +1,43 @@
 #include<stdio.h>
 
-int a, x, b, c, f;
+int a, x, b, c, f, opcao;
 
 void derivada(int fd) {
     
     f = a*(x*x) + b*x + c;
     fd = a*x + b;
     
+    return ;
+
+    printf("SISTEMA DE MÉDIA FINAL");
+    switch (opcao) {
+    case 1:
+    printf("Insira sua pontuação anual");
+    break; 
     
-    return 0;
+    case 2:
+    printf("Calcule sua média");
+    break;
+
+    case 3:
+    printf("Verifique sua situação:");
+    break;
+
+    case 4:
+    printf("Ver o seu resultado");
+    break;
+
+    case 5:
+    printf("Calcule sua derivada");
+    break;
+
+    case 6:
+    printf("Sair");
+    break;
+
+    default:
+    printf("Digite uma das opções acima!");
+
+return 0;
 }
+
