e1b6ea55a2c0c442d6bac1d37d2615ac70eb46cd
Miguel-Marcelo-Oliveira <miguel.marceloramos@gmail.com>
2026-04-23 09:17:08 -0300

    CRIADA INTERFACE QUE RECEBE E GUARDA AS VARIÁVEIS

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
 
-int a, x, b, c, f;
+// PARTE 3 - DERIVADA DE FUNÇÃO
+float a, x, b, c, f;
 
-void derivada(int fd) {
+float derivada(float fd) { // CRIADA A FUNÇÃO QUE DERIVA A FUNÇÃO ORIGINAL --> MIGUEL MARCELO (569467)
     
     f = a*(x*x) + b*x + c;
     fd = a*x + b;
-    
-    
-    return 0;
+    printf("Agora insira os valores da variaveis: \n"); // CRIADA INTERFACE QUE RECEBE E GUARDA AS VARIÁVEIS 
+                                                        // MIGUEL MARCELO (569467)
+    printf("Valor de a: \n");
+    scanf("%f", &a);
+
+    printf("Valor de x: \n");
+    scanf("%f", &x);
+
+    printf("Valor de b: \n");
+    scanf("%f", &b);
+
+    printf("Valor de c: \n");
+    scanf("%f", &c);
+
 }
