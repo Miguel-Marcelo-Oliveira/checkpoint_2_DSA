f112794b148660a333beeb35fae78124e09c48d0
ericx33 <ericxy2007@gmail.com>
2026-04-23 10:40:01 -0300

    Correção do código

--- src/main.c
+++ src/main.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
 // PARTE 2
-void derivada(int a, int x, int b, int c, int f) {
-    
+
+float a, b, c, x, f;
+
+float derivada(){
     f = a*(x*x) + b*x + c;
-    int fd = a*x + b;
-    
+    float fd = 2*a*x + b;
     return fd;
 }
 
 int main(){
-
-int opcao;
+    int opcao;
 
 
 
@@ -46,22 +46,13 @@ int opcao;
 
       
     }
-    return 0; 
-}
 
-// PARTE 3
-#include<stdio.h>
 
 // PARTE 3 - DERIVADA DE FUNÇÃO
-float a, x, b, c, f;
-
-float derivada(){    
-    f = a*(x*x) + b*x + c;
-    return 2*a*x + b;
-}
-
-int main() { // CRIADA A FUNÇÃO QUE DERIVA A FUNÇÃO ORIGINAL --> MIGUEL MARCELO (569467)
+// CRIADA A FUNÇÃO QUE DERIVA A FUNÇÃO ORIGINAL --> MIGUEL MARCELO (569467)
     
+
+
     printf("Agora insira os valores da variaveis: \n"); // CRIADA INTERFACE QUE RECEBE E GUARDA AS VARIÁVEIS 
                                                         // MIGUEL MARCELO (569467)
     printf("Valor de a: \n");
@@ -81,7 +72,8 @@ int main() { // CRIADA A FUNÇÃO QUE DERIVA A FUNÇÃO ORIGINAL --> MIGUEL MARC
     printf("f'(x) = %f\n", fd);
 // CORRIGIDA A FUINÇÃO QUE CALCULA A DERIVADA 
 // E EXIBIÇÃO DO RESULTADO -->  MIGUEL MARCELO (569467)
-    printf("Valor de a: \n");
+
+
 
     return 0;
 }
